b9b195f200c7a4cf230e1f5ff25c806d3b419dd4
G. Kanitz <g.kanitz@linehopper.app>
2020-06-14 23:40:14 +0200

    removed pylint error fix fixed source file paths fixed sql filed names added repartition staep before write

--- dags/transform/immigation.py
+++ dags/transform/immigation.py
@@ -3,18 +3,8 @@ import os
 import datetime as dt
 from pyspark.sql.functions import udf
 
-# define all variables that are provided externally
-# pylint: disable=undefined-variable,self-assigning-variable
-spark = spark
-source_bucket = source_bucket
-output_bucket = output_bucket
-month_year = month_year
-# pylint: enable=undefined-variable,self-assigning-variable
-
 df_spark = spark.read.format("com.github.saurfang.sas.spark").load(
-    os.path.join(
-        source_bucket, f"/raw/i94_immigration_data/i94_{month_year}_sub.sas7bdat"
-    )
+    os.path.join(source_bucket, f"i94_{month_year}_sub.sas7bdat")
 )
 
 # get datetime from arrdate column value
@@ -25,11 +15,13 @@ get_date = udf(
 )
 df_spark = df_spark.withColumn("arrdate", get_date(df_spark.arrdate))
 df_spark.createOrReplaceTempView("df_spark")
-df_usa_states = spark.read(os.path.join(source_bucket, "usa_states.parquet"))
+df_usa_states = spark.read.parquet(os.path.join(output_bucket, "stateCodes.parquet"))
 df_usa_states.createOrReplaceTempView("df_usa_states")
-df_usa_visas = spark.read(os.path.join(source_bucket, "usa_visas.parquet"))
+df_usa_visas = spark.read.parquet(os.path.join(output_bucket, "visaCodes.parquet"))
 df_usa_visas.createOrReplaceTempView("df_usa_visas")
-df_tavel_mode = spark.read(os.path.join(source_bucket, "travel_mode.parquet"))
+df_tavel_mode = spark.read.parquet(
+    os.path.join(output_bucket, "transportCodes.parquet")
+)
 df_tavel_mode.createOrReplaceTempView("df_tavel_mode")
 
 # all missing states are 99, get the arrival mode and the visa type from the mappings
@@ -42,11 +34,11 @@ df_immigration_clean = spark.sql(
         i.i94res as residence_country,
         i.i94port as port,
         i.arrdate as arrival_date,
-        coalesce(m.mode, 'Not reported') as arrival_mode,
-        coalesce(c.state_code, '99') as us_state,
+        coalesce(m.Transport, 'Not reported') as arrival_mode,
+        coalesce(c.State, '99') as us_state,
         i.depdate as departure_date,
         i.i94bir as repondent_age,
-        coalesce(v.visa, 'Other') as visa_type_code,
+        coalesce(v.Visa, 'Other') as visa_type_code,
         i.dtadfile as date_added,
         i.visapost as visa_issued_department,
         i.occup as occupation,
@@ -64,11 +56,11 @@ df_immigration_clean = spark.sql(
     from
         df_spark i
     left join
-        df_usa_states c on i.i94addr=c.state_code
+        df_usa_states c on i.i94addr=c.Code
     left join
-        df_usa_visas v on i.i94visa=v.visa_code
+        df_usa_visas v on i.i94visa=v.Code
     left join
-        df_tavel_mode m on i.i94mode=m.mode_code
+        df_tavel_mode m on i.i94mode=m.Code
 """
 )
 # perform data quality checks
@@ -77,10 +69,14 @@ print(df_immigration_clean.show(5, truncate=False))
 df_immigration_clean.printSchema()
 
 # write data to parquet and partition by year and and month
-dirpath = os.path.join(output_bucket, "immigration.parquet")
-df_immigration_clean.write.mode("overwrite").partitionBy(
+dirpath = os.path.join(output_bucket, "immigration_us_state.parquet")
+df_immigration_clean_repartitioned = df_immigration_clean.repartition(
     "year", "month", "us_state"
-).parquet(dirpath + "_us_state")
-df_immigration_clean.write.mode("overwrite").partitionBy(
+)
+df_immigration_clean_repartitioned.write.mode("overwrite").parquet(dirpath)
+
+dirpath = os.path.join(output_bucket, "immigration_arrival_mode.parquet")
+df_immigration_clean_repartitioned = df_immigration_clean.repartition(
     "year", "month", "arrival_mode", "port"
-).parquet(dirpath + "_arrival_mode")
+)
+df_immigration_clean_repartitioned.write.mode("overwrite").parquet(dirpath)
