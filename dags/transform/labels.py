fc247806e1c27eaadd66a29055153e6e8b691caf
G. Kanitz <g.kanitz@linehopper.app>
2020-06-14 23:43:11 +0200

    using smart_open to read source file directly from s3 type of key and value is decided on first row and fixed there after

--- dags/transform/labels.py
+++ dags/transform/labels.py
@@ -1,11 +1,12 @@
 """Extract all label data into parquet files"""
-import mmap
+import os
 import re
 import pandas as pd
 from fastparquet import write
+from smart_open import open
 
 
-def read_dict(map_file, begin, pattern, key_col_name="Col1", val_col_name="Col2"):
+def read_dict(source_file, begin, pattern, key_col_name="Col1", val_col_name="Col2"):
     """Create dictionary from text stream. Number values are converted to int.
 
     Arguments:
@@ -21,54 +22,79 @@ def read_dict(map_file, begin, pattern, key_col_name="Col1", val_col_name="Col2"
         dict -- Dictionary containing all extracted key value pairs
     """
     values = {key_col_name: [], val_col_name: []}
-    position = map_file.find(begin)
-    if position != -1:
-        map_file.seek(position)
-        map_file.readline()
-        for line in iter(map_file.readline, b""):
-            match = re.match(pattern, line.decode("utf-8"))
+    key_type = None
+    value_type = None
+    with open(source_file, "r") as f:
+        line_number = 0
+        for line in f:
+            line_number = line_number + 1
+            if line.find(begin) != -1:
+                print(f"beginning found on line: {line_number}")
+                break
+        for line in f:
+            match = re.match(pattern, line)
             if match is None:
+                print(f"Read {len(values[key_col_name])} items")
                 break
 
             group = match.groups()
             key = group[0].strip()
-            if key.isdigit():
-                key = int(key)
             value = group[1].strip()
-            if value.isdigit():
+            if key_type is None:
+                if key.isdigit():
+                    key_type = "int"
+                else:
+                    key_type = "str"
+            if value_type is None:
+                if value.isdigit():
+                    value_type = "int"
+                else:
+                    value_type = "str"
+
+            if key_type == "int":
+                key = int(key)
+            if value_type == "int":
                 value = int(value)
             values[key_col_name].append(key)
             values[val_col_name].append(value)
-    return values
+        return values
 
 
-with open("I94_SAS_Labels_Descriptions.SAS", "r") as f:
-    mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+source_file = os.path.join(source_bucket, "I94_SAS_Labels_Descriptions.SAS")
 
-    i94cntyl = read_dict(
-        mm, b"value i94cntyl", r"\s*(\d+)\s*=\s*'{1}(.+)'{1}", "Code", "Country"
-    )
-    df = pd.DataFrame(data=i94cntyl)
-    write("countryCodes.parquet", df)
+print("countryCodes")
+data = read_dict(
+    source_file, "value i94cntyl", r"\s*(\d+)\s*=\s*'{1}(.+)'{1}", "Code", "Country"
+)
+df = pd.DataFrame(data=data)
+write(os.path.join(output_bucket, "countryCodes.parquet"), df, open_with=open)
 
-    i94prtl = read_dict(
-        mm, b"value $i94prtl", r"\s*'{1}(.+)'{1}\s*=\s*'{1}(.+)'{1}", "Code", "Port"
-    )
-    df = pd.DataFrame(data=i94cntyl)
-    write("portCodes.parquet", df)
+print("portCodes")
+data = read_dict(
+    source_file, "value $i94prtl", r"\s*'{1}(.+)'{1}\s*=\s*'{1}(.+)'{1}", "Code", "Port"
+)
+df = pd.DataFrame(data=data)
+write(os.path.join(output_bucket, "portCodes.parquet"), df, open_with=open)
 
-    i94model = read_dict(
-        mm, b"value i94model", r"\s*(\d+)\s*=\s*'{1}(.+)'{1}", "Code", "Transport"
-    )
-    df = pd.DataFrame(data=i94cntyl)
-    write("transportCodes.parquet", df)
+print("transportCodes")
+data = read_dict(
+    source_file, "value i94model", r"\s*(\d+)\s*=\s*'{1}(.+)'{1}", "Code", "Transport"
+)
+df = pd.DataFrame(data=data)
+write(os.path.join(output_bucket, "transportCodes.parquet"), df, open_with=open)
 
-    i94addrl = read_dict(
-        mm, b"value i94addrl", r"\s*'{1}(.+)'{1}\s*=\s*'{1}(.+)'{1}", "Code", "State"
-    )
-    df = pd.DataFrame(data=i94cntyl)
-    write("stateCodes.parquet", df)
+print("stateCodes")
+data = read_dict(
+    source_file,
+    "value i94addrl",
+    r"\s*'{1}(.+)'{1}\s*=\s*'{1}(.+)'{1}",
+    "Code",
+    "State",
+)
+df = pd.DataFrame(data=data)
+write(os.path.join(output_bucket, "stateCodes.parquet"), df, open_with=open)
 
-    i94visa = read_dict(mm, b"I94VISA", r"\s*(\d+)\s*=\s*(\w+)", "Code", "Visa")
-    df = pd.DataFrame(data=i94cntyl)
-    write("visaCodes.parquet", df)
+print("visaCodes")
+data = read_dict(source_file, "I94VISA", r"\s*(\d+)\s*=\s*(\w+)", "Code", "Visa")
+df = pd.DataFrame(data=data)
+write(os.path.join(output_bucket, "visaCodes.parquet"), df, open_with=open)
